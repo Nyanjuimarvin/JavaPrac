5a3650405071303878e128fac38504c5f163ae0a
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-15 12:52:11 +0300

    Food exercise

---
+++ Food/Food.java
@@ -0,0 +1,10 @@
+
+public class Food {
+    public static void main(String[] args) {
+        String[] foods = {"Salted Caramel cake","Applepie","Lasagna","Caviar","Baguettes","Pork chops"};
+        for (String food:foods) {
+            System.out.printf("I am Eating %s\n", food);
+        }
+        System.out.println("I am Full");
+    }
+}
